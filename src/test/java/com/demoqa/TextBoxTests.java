3161cad2d9af1ca91ab7f956ca3076312cf1c53d
Renata Kamalova <renatakamal@gmail.com>
2023-07-11 15:26:21 +0400

    Added TextBoxTests.java

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,27 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests extends TestBase{
+
+    @Test
+        void successfulFillFormTest(){
+            open("/text-box");
+
+            $("#userName").setValue("John");
+            $("#userEmail").setValue("test@email.com");
+            $("#currentAddress").setValue("Nowhere");
+            $("#permanentAddress").setValue("Here");
+            $(byText("Submit")).scrollTo();
+            $("#submit").click();
+
+            $("#output").shouldHave(text("John"), text("test@email.com"), text("Nowhere"), text("Here"));
+
+    }
+
+}
